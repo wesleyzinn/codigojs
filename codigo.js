4d97ee9bad4c5b06204e3060602abbde5df14f44
wesleyzinn <163147282+wesleyzinn@users.noreply.github.com>
2024-08-06 11:40:52 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,15 @@
+//declaração de variaveis
+let quantidade = 0;
+let total = 0;
+
+//função utilizada para calcular a quantidade
+function CalculaPonto(){
+    
+//promp para entrada de dados
+quantidade = parseInt(prompt("digite a quantidades de vezes que foi preciso mandar ele parar de rebolar"));
+
+total = total + quantidades;
+
+//Mostra o resultado usando alert
+alert(total);
+};
