0a165a26838ffea55e4964de7d5214b92d426c65
wesleyzinn <163147282+wesleyzinn@users.noreply.github.com>
2024-08-15 12:09:24 -0300

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,15 +1,26 @@
-//declaração de variaveis
-let quantidade = 0;
-let total = 0;
+let tentativa = 0;
+let min = 1;
+let max = 100;
+let dif = max - min;
+let aleatorio = Math.random();
+let resultado = min + Math.trunc(dif * aleatorio);
 
-//função utilizada para calcular a quantidade
-function CalculaPonto(){
-    
-//promp para entrada de dados
-quantidade = parseInt(prompt("digite a quantidades de vezes que foi preciso mandar ele parar de rebolar"));
+function AdivinhaNumero() {
+    tentativa = prompt("Digite a sua tentativa");
 
-total = total + quantidades;
+    let dados = window.document.getElementById('tentativas');
 
-//Mostra o resultado usando alert
-alert(total);
-};
+    if(tentativa == resultado){
+        dados.innerHTML += "<p> Parabéns, o número que eu pensei é esse: " + tentativa + ".<p>"
+        document.getElementById('botao').style.visibility = 'hidden';
+    } else if(tentativa < resultado){
+        dados.innerHTML += "<p> Você falou " + tentativa + ". Meu número é <b>MAIOR</b><p>"
+    } else if(tentativa > max){
+        dados.innerHTML += "<p> <b>Digite um número entre 1 a 100.</b><p>"
+    } else if(tentativa > resultado){
+        dados.innerHTML += "<p>Vocẽ falou " + tentativa + ". Meu número é MENOR<p>"
+    } 
+
+
+
+}
